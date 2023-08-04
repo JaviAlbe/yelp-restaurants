5b9554aaa64188eea6d091d9a00b35a758f68686
Javier Albeniz <j.albenizlopez@gmail.com>
2023-08-04 18:46:16 +0100

    Working on the selected restaurant information

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,16 +1,54 @@
-import React from "react";
-import {View, Image, Text, StyleSheet } from "react-native";
+import React, {useEffect, useState} from "react";
+import {View, Image, Text, StyleSheet, FlatList} from "react-native";
+import yelp from '../api/yelp'
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+
+    const [result, setResult] =  useState(null)
+
+    //Instead of through props, the id is passed using the navigation library and extracted here to show
+    //the correct selected restaurant
+    const id = navigation.getParam('id')
+
+    //Call to the Yelp API to get the info from the correct restaurant
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`)
+        setResult(response.data)
+    }
+
+    useEffect(() => {
+        getResult(id)
+    }, [])
+
+    //don't render anything until results is no longer null
+    if(!result) {
+        return(
+            <View>
+                <Text>Trying to fetch restaurant information...</Text>
+            </View>
+        )
+    }
+
     return(
         <View>
-            <Text>Hello</Text>
+            <Text>{result.name}</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({ item }) => {
+                    return(
+                        <Image style={styles.image} source={{uri: item}} />
+                    )
+                }}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    image: {
+        height: 220,
+        width: 300,
+    }
 })
 
 export default ResultsShowScreen
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
-import {View, Text, StyleSheet, ScrollView} from "react-native";
+import { Text, ScrollView} from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
 import ResultsList from "../components/ResultsList";
 
-const SearchScreen = ({ navigation }) => {
+const SearchScreen = ( ) => {
 
     const [term, setTerm] = useState('')
     const [searchApi, results, errorMessage] = useResults()
@@ -26,9 +26,9 @@ const SearchScreen = ({ navigation }) => {
             {errorMessage ? <Text>{errorMessage}</Text> : null}
 
             <ScrollView>
-                <ResultsList navigation={navigation} results={filterResultsByPrice('$')} title='Cost Effective' />
-                <ResultsList navigation={navigation} results={filterResultsByPrice('$$')} title='Bit Pricier'/>
-                <ResultsList navigation={navigation} results={filterResultsByPrice('$$$')} title='Big Spender'/>
+                <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
+                <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier'/>
+                <ResultsList  results={filterResultsByPrice('$$$')} title='Big Spender'/>
             </ScrollView>
         </>
     )
